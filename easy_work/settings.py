33976a892f86422ef67d9c6fe1e8ed5db465db8c
Aleksandr <Al.Melnikov2@gmail.com>
2023-02-22 14:12:59 +0200

    change CORS settings

--- easy_work/settings.py
+++ easy_work/settings.py
@@ -157,8 +157,8 @@ LOGOUT_REDIRECT_URL = '/'
 
 
 #  CORS
-#  CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_ALL_ORIGINS = True
 
-CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3030',
-]
+# CORS_ALLOWED_ORIGINS = [
+#     'http://localhost:3030',
+# ]
